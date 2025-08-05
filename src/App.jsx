c5cd1802fb0592d3456c4431fe8557b2d7fe2d73
Gunavardhan06 <gunavardhan599@gmail.com>
2025-08-05 19:46:45 +0530

    Updated Sidebar and App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from './Components/sidebar';
 import Feed from './Components/Feed';
 import Suggestions from './Components/Suggestions';
 import StoryBar from './Components/StoryBar';
+// import instagram_txt from './instagram_txt.png';
 
 function App() {
   return (
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Sidebar.css";
+import myImage from "../assets/Instagram_txt.png";
 
 function Sidebar() {
   return (
     <div className="sidebar d-flex flex-column justify-content-between p-3 min-vh-100">
       <div>
-        <h4 className="mb-4">Instagram</h4>
+        <img src={myImage} alt="Instagram Logo" className="mb-4" style={{ width: '120px' }} />
+
         <div className="mb-3"><i className="bi bi-house-door-fill me-2"></i>Home</div>
         <div className="mb-3"><i className="bi bi-search-heart-fill me-2"></i>Search</div>
         <div className="mb-3"><i className="bi bi-play-circle-fill me-2"></i>Reels</div>
